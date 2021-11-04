cdca2868ae71928983ed49bdaa60dbbfae93e037
EEduardoG <85304409+EEduardoG@users.noreply.github.com>
2021-11-04 11:55:30 -0600

    Create helloworld.php

---
+++ helloworld.php
@@ -0,0 +1,24 @@
+
+
+<?php 
+/*** 
+ * Author: Erick García
+ * Date: 03 November 2021
+ * Description: A simple hello world with some basic information about
+ * how construct a simple php file, define a variable and print
+ * information with echo.
+ * 
+ * ***/
+/** All php files inits with '<php' and ends with '?>' ***/
+
+//with echo. We can print values on screen.
+echo 'Hello World';
+
+//This is a variable. It serves to store a value in memory that we can use to interact.
+$message = 'Hello world'; 
+echo $message;
+
+//As you can see. All lines in PHP ends with a semicolon. DONT FORGET IT.
+
+
+?>
